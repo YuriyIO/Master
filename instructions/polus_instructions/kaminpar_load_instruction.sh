9df3a02feab8482cf79e848f6124609ec5a7f821
YuriyIO <Yuriy.Maksimovish@yandex.ru>
2026-05-21 19:13:53 +0300

    commit

--- instructions/polus_instructions/kaminpar_load_instruction.sh
+++ instructions/polus_instructions/kaminpar_load_instruction.sh
@@ -16,25 +16,96 @@ make install
 
 # KaMinPar
 
+SPARSE_PATH="/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/local_libs/sparsehash/include"
+
+
+# Обновляем обертку для C
+echo '#!/bin/bash
+ARGS=()
+for arg in "$@"; do
+    if [[ "$arg" == "-march=native" ]]; then
+        ARGS+=("-mcpu=native")
+    elif [[ "$arg" == "-fcf-protection=full"  "$arg" == "-fcf-protection" ]]; then
+        continue
+    else
+        ARGS+=("$arg")
+    fi
+done
+exec /home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/gcc-13.4.0/bin/gcc "-I'$SPARSE_PATH'" "${ARGS[@]}"' > /home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/kaminpar_fix/kaminpar_cc
+
+# Обновляем обертку для C++
+echo '#!/bin/bash
+ARGS=()
+for arg in "$@"; do
+    if [[ "$arg" == "-march=native" ]]; then
+        ARGS+=("-mcpu=native")
+    elif [[ "$arg" == "-fcf-protection=full"  "$arg" == "-fcf-protection" ]]; then
+        continue
+    else
+        ARGS+=("$arg")
+    fi
+done
+exec /home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/gcc-13.4.0/bin/g++ "-I'$SPARSE_PATH'" "${ARGS[@]}"' > /home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/kaminpar_fix/kaminpar_cxx
+
+chmod +x /home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/kaminpar_fix/kaminpar_cc
+chmod +x /home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/kaminpar_fix/kaminpar_cxx
+
+
 git clone https://github.com/KaHIP/KaMinPar.git
 
 cd KaMinPar
 
-cmake -B build --preset=distributed \
-  -DCMAKE_PREFIX_PATH="/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/local_libs/sparsehash" \
-  -DCMAKE_INSTALL_PREFIX="/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/local_libs/KaMinPar"
+sed -i 's/#error "Only x64 and ARM are supported"/\/\/#error "PowerPC fallback"/' kaminpar-common/graph_compression/streamvbyte.h
+
+cat << 'EOF' > kaminpar-common/graph_compression/streamvbyte.h
+#pragma once
+#include <cstdint>
+#include <vector>
+
+namespace kaminpar::streamvbyte {
+    enum class DifferentialCodingKind { NONE, D1, D2, D4, DM };
+
+    // auto... Args позволяет принимать и типы (NodeID), и значения (false, D1)
+    template <typename T, auto... Args>
+    struct UniversalStub {
+        UniversalStub(auto...) {}
+        template <typename L> void decode(L&&) {}
+        std::size_t add(auto...) { return 0; }
+        void flush() {}
+        void finalize() {}
+    };
+
+    template <typename T, auto... Args> 
+    using StreamVByteDecoder = UniversalStub<T, Args...>;
+    
+    template <typename T, auto... Args> 
+    using StreamVByteEncoder = UniversalStub<T, Args...>;
+}
+
+namespace kaminpar {
+    template <typename T, auto... Args> 
+    using StreamVByteGapAndWeightsDecoder = streamvbyte::UniversalStub<T, Args...>;
+    
+    template <typename T, auto... Args> 
+    using StreamVByteGapDecoder = streamvbyte::UniversalStub<T, Args...>;
+    
+    template <typename T, auto... Args> 
+    using StreamVByteGapAndWeightEncoder = streamvbyte::UniversalStub<T, Args...>;
+    
+    template <typename T, auto... Args> 
+    using StreamVByteGapEncoder = streamvbyte::UniversalStub<T, Args...>;
+}
+EOF
 
 
 cmake -B build --preset=distributed \
-  -DCMAKE_C_FLAGS="-mcpu=native" \
-  -DCMAKE_CXX_FLAGS="-mcpu=native" \
-  -DKAMINPAR_BUILD_TESTS=OFF \
+  -DCMAKE_CXX_COMPILER=/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/kaminpar_fix/kaminpar_cxx \
+  -DCMAKE_C_COMPILER=/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/kaminpar_fix/kaminpar_cc \
   -DCMAKE_PREFIX_PATH="/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/local_libs/sparsehash" \
   -DCMAKE_INSTALL_PREFIX="/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/local_libs/KaMinPar"
-
-
-find build -type f -exec sed -i 's/-march=native/-mcpu=native/g' {} +
 
 cmake --build build --parallel 8
 
 cmake --install build
+
+ln -s lib64 lib
--- instructions/polus_instructions/scotch_load_instruction.sh
+++ instructions/polus_instructions/scotch_load_instruction.sh
@@ -2,10 +2,12 @@ git clone https://gitlab.inria.fr/scotch/scotch.git
 
 cd scotch/ &&  mkdir build && cd build/
 
-cmake .. -DCMAKE_INSTALL_PREFIX=/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/local_libs/scotch
+cmake .. -DCMAKE_INSTALL_PREFIX=/home_edu/edu-cmc-sqi23/edu-cmc-sqi23-25/local_libs/scotch     -DCMAKE_C_FLAGS="-D_SCOTCHyylex=yylex -D_SCOTCHyyparse=yyparse -D_SCOTCHyyerror=yyerror -D_SCOTCHyylval=yylval -D_SCOTCHyychar=yychar -D_SCOTCHyydebug=yydebug -D_SCOTCHyynerrs=yynerrs"
 
-make -j8
+make -j1
 
 make install
+
+ln -s lib64 lib
 
 cd ..
